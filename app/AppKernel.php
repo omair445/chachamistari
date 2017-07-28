b6f54671ab20ecdba219491e1a315415c5cac6d5
DESKTOP-ETVAG42\omair <omair.thedevclan@gmail.com>
2017-07-28 16:18:18 -0700

    app kernal modified (remvoved dev env )

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -23,10 +23,10 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
 
-            if ('dev' === $this->getEnvironment()) {
-                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
-                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
-            }
+//            if ('dev' === $this->getEnvironment()) {
+//                $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+//                $bundles[] = new Symfony\Bundle\WebServerBundle\WebServerBundle();
+//            }
         }
 
         return $bundles;
@@ -39,16 +39,16 @@ class AppKernel extends Kernel
 
     public function getCacheDir()
     {
-        return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
+        return dirname(__DIR__) . '/var/cache/' . $this->getEnvironment();
     }
 
     public function getLogDir()
     {
-        return dirname(__DIR__).'/var/logs';
+        return dirname(__DIR__) . '/var/logs';
     }
 
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
-        $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
+        $loader->load($this->getRootDir() . '/config/config_' . $this->getEnvironment() . '.yml');
     }
 }
